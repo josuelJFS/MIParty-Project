dd395d020fbf091367465e366900c0cd02299797
josuelJFS <josuelafk@gmail.com>
2023-12-07 13:06:01 -0300

    adjustes

--- src/service/apiAxios.tsx
+++ src/service/apiAxios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 const api = axios.create({
-  baseURL: "http://seuIP:3333/",
+  baseURL: "http://192.168.100.30:3333/",
 });
 
 api.interceptors.request.use(async (config: any) => {
